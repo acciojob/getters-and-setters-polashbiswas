5e95e0085ed7ae38719258fa43e6c5ce653c8200
polashbiswas <30291056+polashbiswas@users.noreply.github.com>
2023-04-23 14:26:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-//complete this code
 class Person {
-	constructor(name, age){
-		this.name = name;
-		this.age = age;
+	constructor(name,age){
+		this._name = name;
+		this._age = age;
 	}
-	get getName(){
-		return this.name
+
+	get name(){
+		return this._name;
 	}
-	set setAge(age){
-		this.age = age;
+
+	set age(age){
+		this._age = age;
 	}
 }
 
 class Student extends Person {
 	study(){
-		console.log(this.name + " is studying");
+		console.log(this._name + " is studying");
 	}
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(this.name + " is teaching");
+		console.log(this._name + " is teaching");
 	}
 }
 
