f1f2a68e79fec2fc771388711e82aafb3a251158
polashbiswas <30291056+polashbiswas@users.noreply.github.com>
2023-04-23 14:00:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ class Person {
 
 class Student extends Person {
 	study(){
-		console.log(`${name} is studying`);
+		console.log(this.name + " is studying");
 	}
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(`${name} is teching`);
+		console.log(this.name + " is teaching");
 	}
 }
 
